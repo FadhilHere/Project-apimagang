9e6960001b08e5cf30f7a6b05d24b4a756e3ad82
FadhilHere <88182212+FadhilHere@users.noreply.github.com>
2021-11-05 14:52:07 +0700

    filter jika ada kode matkul yg sama

--- app/controllers/matkul.controller.js
+++ app/controllers/matkul.controller.js
@@ -24,7 +24,7 @@ exports.create = (req, res) => {
           });
         });
     } else {
-      res.send({ message: "Matkul already exists " });
+      res.send({ message: "Kode already exists " });
     }
   });
   // Save Matakuliah in the database
