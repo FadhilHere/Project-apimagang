da9e98527f75201ae4ee4f98a3450c432b5c4f42
FadhilHere <88182212+FadhilHere@users.noreply.github.com>
2021-11-12 14:56:11 +0700

    pls bisa

--- app/controllers/mahasiswa.controller.js
+++ app/controllers/mahasiswa.controller.js
@@ -131,19 +131,3 @@ exports.delete = (req, res) => {
 exports.deleteAll = (req, res) => {};
 
 exports.findAllPublished = (req, res) => {};
-
-exports.findByKelas = (req, res) => {
-  Mahasiswa.find({
-    $match: {
-      id_kelas: req.query.kelas,
-    },
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving.",
-      });
-    });
-};
