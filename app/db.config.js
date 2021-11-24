0673094296d33e348649ff3564a33e2449ecb89a
FadhilHere <88182212+FadhilHere@users.noreply.github.com>
2021-11-24 09:15:07 +0700

    upload foto

--- app/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  //url: `mongodb+srv://FadhilTesting:RSnKtLQr9LLkLh26@cluster0.gcoyp.mongodb.net/Project?retryWrites=true&w=majority`,
-  url: `mongodb://FadhilTesting:RSnKtLQr9LLkLh26@cluster0-shard-00-00.gcoyp.mongodb.net:27017,cluster0-shard-00-01.gcoyp.mongodb.net:27017,cluster0-shard-00-02.gcoyp.mongodb.net:27017/Project?ssl=true&replicaSet=atlas-orbbz0-shard-0&authSource=admin&retryWrites=true&w=majority`,
-};
